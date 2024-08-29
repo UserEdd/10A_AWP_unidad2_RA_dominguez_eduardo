b1de5848e76351a6709bc6c39305f7846dca0ebd
john0999 <moralesgomezjohnny@gmail.com>
2024-08-29 15:15:12 -0600

    cambios en db:seed users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,16 @@ return new class extends Migration
             // $table->rememberToken();
             // $table->foreignId('current_team_id')->nullable();
             // $table->string('profile_photo_path', 2048)->nullable();
+
+        DB::table('users')->insert([
+                'name' => 'Juan',
+                'lastname' => 'Ruíz',
+                'email' => 'juanruiz@gmail.com',
+                'password' => Hash::make('Admin123'), // Encriptar la contraseña
+                'estado' => 'active',
+                'created_at' => now(),
+                'updated_at' => now(),
+        ]);
     }
 
     /**
