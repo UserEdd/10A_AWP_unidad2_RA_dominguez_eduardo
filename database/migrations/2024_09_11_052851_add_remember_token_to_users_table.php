4136973f887f5edf164603dd394fe6c2cb6f0202
josaguapo7 <josafatrujillo604@gmail.com>
2024-09-10 23:34:00 -0600

    actualizacion de session activa.

---
+++ database/migrations/2024_09_11_052851_add_remember_token_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Agregar la columna remember_token a la tabla users
+            $table->rememberToken()->after('password'); // Añade la columna después del campo 'password'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Eliminar la columna remember_token si se revierte la migración
+            $table->dropColumn('remember_token');
+        });
+    }
+};
