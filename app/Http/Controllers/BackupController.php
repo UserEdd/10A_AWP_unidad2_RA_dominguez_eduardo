405599971ee08f3e86fa83817a822a886f18bd9e
UserEdd <eduardodominguez@laselva.edu.mx>
2024-09-13 18:37:29 -0600

    Actualizacion de controlador 1.1

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -9,7 +9,7 @@ class BackupController extends Controller
     public function Backup()
     {
         $fecha = date('d-m-Y');
-        $nombreArchivo = 'Backup-SOCIALERT' . $fecha . '.zip';
+        $nombreArchivo = 'Backup-SOCIALERT ' . $fecha . '.zip';
 
         $comando = "php " . base_path() . "/artisan backup:run --only-db --filename=" . $nombreArchivo;
         exec($comando);
