3ee490fe91e6dd9fb3d11e9be9a40417f7a3c002
UserEdd <eduardodominguez@laselva.edu.mx>
2024-09-13 18:33:21 -0600

    Actualizacion de controlador

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
 
 class BackupController extends Controller
 {
@@ -13,23 +10,12 @@ class BackupController extends Controller
     {
         $fecha = date('d-m-Y');
         $nombreArchivo = 'Backup-SOCIALERT' . $fecha . '.zip';
-        
-        // Llamada al comando Artisan de forma interna, sin necesidad de exec()
-        Artisan::call('backup:run', ['--only-db' => true, '--filename' => $nombreArchivo]);
 
-        // Registrar salida en los logs
-        Log::info('Backup ejecutado con nombre de archivo: ' . $nombreArchivo);
+        $comando = "php " . base_path() . "/artisan backup:run --only-db --filename=" . $nombreArchivo;
+        exec($comando);
 
-        // Verificar la ruta del archivo
         $rutaArchivo = storage_path('app/Laravel/' . $nombreArchivo);
 
-        // Verificar si el archivo existe
-        if (Storage::exists('Laravel/' . $nombreArchivo)) {
-            Log::info('Archivo encontrado: ' . $rutaArchivo);
-            return response()->download($rutaArchivo)->deleteFileAfterSend();
-        } else {
-            Log::info('Archivo no encontrado: ' . $rutaArchivo);
-            return response()->json(['message' => 'Backup file not found.'], 404);
-        }
+        return response()->download($rutaArchivo)->deleteFileAfterSend();
     }
 }
