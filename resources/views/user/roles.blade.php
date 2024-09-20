d06209d7f2ca8bce396bfafeabf189648ea52a21
john0999 <moralesgomezjohnny@gmail.com>
2024-09-20 01:17:15 -0600

    Autenticacion roles

--- resources/views/user/roles.blade.php
+++ resources/views/user/roles.blade.php
@@ -21,7 +21,7 @@
     @endif
     @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('message') }}
+            {{ session('success') }}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
